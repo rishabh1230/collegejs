6e33ad43211bcee9321a557f1294d31f597bdf6c
rishabh1230 <rishabhkis0710@gmail.com>
2025-04-23 15:51:38 +0530

    Added maps

---
+++ map.js
@@ -0,0 +1,47 @@
+//SET METHOD
+// const fruits=new Map();
+// fruits.set("apple",100);
+// fruits.set("banana",200);
+// fruits.set("orange",300);
+// console.log(fruits);
+
+// GET ,METHOD
+// const fruits=new Map();
+// fruits.set("apple",100);
+// fruits.set("banana",200);
+// fruits.set("orange",300);
+// console.log(fruits.get("apple"));
+// console.log(fruits.get("banana"));
+// console.log(fruits.get("orange"));   
+
+
+// NEW Map
+
+// const fruits=new Map([["apple",100],["banana",200],["orange",300]]);
+// console.log(fruits);    
+
+// MAP SIZE
+// const fruits=new Map([["apple",100],["banana",200],["orange",300]]);
+// console.log(fruits.size);
+
+// MAP DELETE
+// const fruits=new Map([["apple",100],["banana",200],["orange",300]]);
+// fruits.delete("apple");
+// console.log(fruits);
+
+// MAP CLEAR
+// const fruits=new Map([["apple",100],["banana",200],["orange",300]]);
+// fruits.clear();
+// console.log(fruits);
+
+// MAP Has
+
+// const fruits=new Map([["apple",100],["banana",200],["orange",300]]);  
+// console.log(fruits.has("apple"));    
+
+// MAP FOR EACH
+// let text="";
+// fruits.forEach(fucntion(value,key){
+//     text+=key+" is "+value;
+    
+// })
